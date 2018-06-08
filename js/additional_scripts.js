1534874114262cf75941926371f02132044824ca
postolenko <denis.postolenko@gmail.com>
2018-06-08 18:11:39 +0300

    Batega

--- js/additional_scripts.js
+++ js/additional_scripts.js
@@ -129,7 +129,40 @@ $(document).ready(function() {
             slidesToShow: 4,
             slidesToScroll: 1,
             focusOnSelect: true,
-            asNavFor: bigSlider
+            asNavFor: bigSlider,
+            responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+              }
+            },
+            {
+              breakpoint: 900,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 2,
+                // centerMode: true
+              }
+            },
+            {
+              breakpoint: 520,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                // centerMode: true
+              }
+            },
+            {
+              breakpoint: 380,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                // centerMode: false
+              }
+            }
+          ]
         });
 
     });
